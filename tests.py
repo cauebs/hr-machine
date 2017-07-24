b80864034b830b3bd3748f1f53a6598f1151a771
Caue Baasch de Souza <cauebaasch@gmail.com>
2017-07-24 00:10:17 -0300

    Added some more examples and tests

--- tests.py
+++ tests.py
@@ -35,5 +35,60 @@ def test_alphabetizer():
     assert outbox == string_to_values('mypy')
 
 
+def test_digit_exploder():
+    registers = [None] * 12
+    registers[9] = 0
+    registers[10] = 10
+    registers[11] = 100
+
+    machine = Machine(registers)
+
+    outbox = machine.run_file('examples/digit-exploder.hr',
+                              inbox=[9, 809, 838, 66])
+    assert outbox == [9, 8, 0, 9, 8, 3, 8, 6, 6]
+
+    outbox = machine.run_file('examples/digit-exploder.hr',
+                              inbox=[819, 14, 544, 92],
+                              registers=registers)
+    assert outbox == [8, 1, 9, 1, 4, 5, 4, 4, 9, 2]
+
+    outbox = machine.run_file('examples/digit-exploder.hr',
+                              inbox=[638, 504, 5, 21, 33],
+                              registers=registers)
+    assert outbox == [6, 3, 8, 5, 0, 4, 5, 2, 1, 3, 3]
+
+
+def test_vowel_incinerator():
+    registers = [None] * 10
+    registers[0:5] = string_to_values('aeiou')
+    registers[5] = 0
+
+    machine = Machine(registers)
+    outbox = machine.run_file('examples/vowel-incinerator.hr',
+                              inbox=string_to_values('AOJEQOQDJTNJQEBNEJCJ'))
+    assert outbox == string_to_values('JQQDJTNJQBNJCJ')
+
+
+def test_string_reverse():
+    registers = [None] * 15
+    registers[14] = 0
+
+    machine = Machine(registers)
+
+    inbox = string_to_values('cauebs python', zero_terminated=True)
+    outbox = machine.run_file('examples/string-reverse.hr', inbox)
+    assert outbox == string_to_values('sbeuacnohtyp')
+
+    inbox = string_to_values('human resource', zero_terminated=True)
+    outbox = machine.run_file('examples/string-reverse.hr',
+                              inbox, registers=registers)
+    assert outbox == string_to_values('namuhecruoser')
+
+    inbox = string_to_values('mypy pytest', zero_terminated=True)
+    outbox = machine.run_file('examples/string-reverse.hr',
+                              inbox, registers=registers)
+    assert outbox == string_to_values('ypymtsetyp')
+
+
 if __name__ == '__main__':
     test_alphabetizer()
